89a78896c12be1228a78a3e6898b327818a55548
sbmdkl <sbmdkl@gmail.com>
2021-03-09 14:00:10 +0545

    updated mobiles DAL

--- components/mobiles/mobilesDAL.js
+++ components/mobiles/mobilesDAL.js
@@ -6,23 +6,39 @@ const create = async (newMobile) => {
 };
 
 const findAll = async ({ query }) => {
-	let { skip, limit } = query;
-	skip = skip ? Number(skip) : 0;
-	limit = limit ? Number(limit) : 10;
-	return await Mobile.find({}).skip(skip).limit(limit).sort('-date');
+	try {
+		let { skip, limit } = query;
+		skip = skip ? Number(skip) : 0;
+		limit = limit ? Number(limit) : 10;
+		return await Mobile.find({}).skip(skip).limit(limit).sort('-date');
+	} catch (e) {
+		throw { error: 'Unable to handle request' };
+	}
 };
 
 const findOneById = async (id) => {
-	const mobile = await Mobile.findById(id);
-	return mobile.toObject();
+	try {
+		const mobile = await Mobile.findById(id);
+		return mobile.toObject();
+	} catch (e) {
+		throw { error: 'Unable to handle request' };
+	}
 };
 
 const findByIdAndUpdate = async ({ id, updateMobile }) => {
-	return await Mobile.findByIdAndUpdate(id, updateMobile, { new: true, runValidators: true });
+	try {
+		return await Mobile.findByIdAndUpdate(id, updateMobile, { new: true, runValidators: true });
+	} catch (e) {
+		throw { error: 'Unable to handle request' };
+	}
 };
 
 const findByIdAndRemove = async (id) => {
-	return await Mobile.findByIdAndRemove(id);
+	try {
+		return await Mobile.findByIdAndRemove(id);
+	} catch (e) {
+		throw { error: 'Unable to handle request' };
+	}
 };
 
 module.exports = {
