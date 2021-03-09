ccf12d4023c2e5df9e64bc9cf14413f0b6cd9324
sbmdkl <sbmdkl@gmail.com>
2021-03-09 16:31:43 +0545

    Footer Component UI

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Footer.module.css';
+export default function Footer() {
+	return (
+		<footer className={styles['footer']}>
+			<p>2020 © Mobile Shop</p>
+		</footer>
+	);
+}
