2492005e0edb38013834c0b21877bb01975d8084
sbmdkl <sbmdkl@gmail.com>
2021-03-09 14:04:46 +0545

    rename database

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 module.exports = async () => {
 	mongoose
-		.connect('mongodb://localhost:27017/boilerplate', {
+		.connect('mongodb://localhost:27017/mobiles', {
 			useNewUrlParser: true,
 			useFindAndModify: false,
 			useCreateIndex: true,
