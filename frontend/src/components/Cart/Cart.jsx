18c15d98e906dc659a0eb55ea0c23c59ed4f28e5
sbmdkl <sbmdkl@gmail.com>
2021-03-09 17:23:45 +0545

    Added CartDropdown UI

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -3,9 +3,37 @@ import styles from './Cart.module.css';
 
 export default function Cart() {
 	return (
-		<div className={styles['cart']}>
-			<i className='fas fa-shopping-cart' />
-			Cart (0)
-		</div>
+		<React.Fragment>
+			<div className={styles['cart']}>
+				<i className='fas fa-shopping-cart' />
+				Cart (0)
+				<div className={`${styles['cartDropdown']}`}>
+					<div className={`${styles['cartItem']}`}>
+						<div className={`${styles['cartItemName']}`}>
+							<p>Iphone</p>
+							<button className='btn btn-sm btn-danger'>X</button>
+						</div>
+						<div>
+							<p>Quantity 1</p>
+							<p>price Rs.100</p>
+							<p>total Rs.100</p>
+						</div>
+						<hr />
+					</div>
+					<div className={`${styles['cartItem']}`}>
+						<div className={`${styles['cartItemName']}`}>
+							<p>Iphone</p>
+							<button className='btn btn-sm btn-danger'>X</button>
+						</div>
+						<div>
+							<p>Quantity 1</p>
+							<p>price Rs.100</p>
+							<p>total Rs.100</p>
+						</div>
+						<hr />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
 	);
 }
