c4b7300e9fdb0795ba1bf282d24ee3f2fc25fffe
sbmdkl <sbmdkl@gmail.com>
2021-03-10 12:03:52 +0545

    404-route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@
 const mobilesRoute = require('../components/mobiles/mobilesAPI');
 const init = (app) => {
 	app.use('/api/mobiles', mobilesRoute);
+	app.all('*', (req, res) => {
+		res.status(404).json({
+			status: 'error',
+			path: req.originalUrl,
+			message: `The app you are using is not latest, for this service you need to update your application.`,
+			systemTime: Date.now()
+		});
+	});
 };
 
 module.exports = {
